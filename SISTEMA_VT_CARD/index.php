7dfc4a8a3b34810bc6c71238f72dcc98007e70af
Fernando <fernando.sversiani@gmail.com>
2017-03-30 11:50:02 -0300

    VERIFY
    TESTES DE INTEGRACAO GIT

---
+++ SISTEMA_VT_CARD/index.php
@@ -0,0 +1,13 @@
+<html>    
+    <head>
+        <title>
+            MENU - SITE
+        </title>
+        <link rel="stylesheet" type="text/css" href="modelo_style.css" />
+    </head>
+            <div class="conteudo">
+            <br> Fernando..
+        </div>
+        
+    </body>
+</html>
