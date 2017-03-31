e7b7e291835ead4c04108ceea79a5bb988a8274a
Fernando <fernando.sversiani@gmail.com>
2017-03-31 15:38:06 -0300

    PAGINA_LOGIN_AZUL
    PAGINA COM OS SHADOW AZUL

--- SISTEMA_VT_CARD/index.php
+++ SISTEMA_VT_CARD/index.php
@@ -1,13 +1,22 @@
 <html>    
     <head>
-        <title>
-            MENU - SITE
-        </title>
-        <link rel="stylesheet" type="text/css" href="modelo_style.css" />
+        <title> Login - Control VT Card </title>
+        <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,600,700" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href=CSS/style.css>
     </head>
-            <div class="conteudo">
-            
-            <br> Fernando..
+    <body>
+        <div id="login" class="bradius">
+            <div class="message"></div>
+            <div class="logo"><a href="<?php echo $home;?>" title="<?php echo $title;?>"><img src="css/imagens/log_vt_card.png" alt="<?php echo $title;?>" title="<?php echo $title;?>" width="150" height="60" /></a></div>
+            <div class="acomodar">
+                <form action="" method="post">
+                    <label for="email">E-mail</label><input id="email" type="email" class="txt bradius" name="email" value=""</>
+                    <label for="senha">Senha</label><input id="senha" type="password" class="txt bradius" name="senha" value=""</>
+                    <input type="submit" class="sb bradius" value="ENTRAR"/>
+                </form>
+            <!--ACOMODAR-->
+            </div>
+        <!--LOGIN-->    
         </div>
         
     </body>
