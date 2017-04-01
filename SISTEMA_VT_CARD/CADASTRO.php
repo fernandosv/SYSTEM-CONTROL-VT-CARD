26f1fd52442512ace63977b2cbdb256455196e5f
Fernando <fernando.sversiani@gmail.com>
2017-03-31 21:35:18 -0300

    RAQUO TOPO
    MAS OPCAO CADASTRO

--- SISTEMA_VT_CARD/CADASTRO.php
+++ SISTEMA_VT_CARD/CADASTRO.php
@@ -1,25 +1,26 @@
 <html>    
     <head>
-        <title> Login - Control VT Card </title>
+        <title> CADASTRO - Control VT Card </title>
         <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,600,700" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href=CSS/style.css>
     </head>
     <body>
-        <div id="cadastrar"><a href="cadastro.php" title="Cadastre-se">Cadastre-se &raquo; </a></div>
-        <div id="login" class="bradius">
+        <div id="cadastrar"><a href="index.php" title="Cadastre-se">Login &raquo; </a></div>
+        <div id="login" class="bradius" style="top: 120px;">
             <div class="message"></div>
             <div class="logo"><a href="<?php echo $home;?>" title="<?php echo $title;?>"><img src="/imagens/log_vt_card.png" alt="<?php echo $title;?>" title="<?php echo $title;?>" width="150" height="60" /></a></div>
             <div class="acomodar">
                 <form action="" method="post">
+                    <label for="nome">Nome</label><input id="nome" type="text" class="txt bradius" name="nome" value=""</>
+                    <label for="end">Endereço</label><input id="end" type="email" class="txt bradius" name="end" value=""</>
                     <label for="email">E-mail</label><input id="email" type="email" class="txt bradius" name="email" value=""</>
                     <label for="senha">Senha</label><input id="senha" type="password" class="txt bradius" name="senha" value=""</>
-                    <input type="submit" class="sb bradius" value="ENTRAR"/>
+                    <input type="submit" class="sb bradius" value="CADASTRAR"/>
                 </form>
             <!--ACOMODAR-->
             </div>
         <!--LOGIN-->    
         </div>
-       
+        
     </body>
 </html>
-
