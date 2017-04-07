fa2b80e7e0ce218ecfba10ea789723a50ce8e85f
Fernando <fernando.sversiani@gmail.com>
2017-04-07 08:49:03 -0300

    insert home_logado

---
+++ SISTEMA_VT_CARD/HOME.php
@@ -0,0 +1,78 @@
+<html>    
+    <head>
+        <title> LogADO - Control VT Card </title>
+        <link rel="stylesheet" type="text/css" href="css/style_home_logado.css">
+        <link rel="stylesheet" type="text/css" href="">
+            <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+            <!-- Optional theme -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+            <!-- Latest compiled and minified JavaScript --><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>        
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+            <!-- Latest compiled and minified JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top"  >
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu_header"
+                    aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                    </button>
+                    
+                    <a class="navbar-brand" href="index.php">
+                        <img src="css/imagens/log_vt_card.png"
+                    </a>
+                </div>
+                
+                <div id="menu_header" class="navbar-collapse collapse">
+                    
+				<ul class="nav navbar-nav">
+					<li>
+						<a href="#">
+							otimo
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							bhsbus
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							setealagoano
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							betim card
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							envio
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							status
+						</a>
+					</li>
+					<li>
+						<a href="#">
+                                                    Suporte</a>
+                                        </li>
+                                </ul>
+                </div>
+            </div>
+        </nav>
+        <div>
+            
+        </div>
+    </body>
+</html>
